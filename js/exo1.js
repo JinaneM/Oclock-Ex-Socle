4c614952a0db7f67e045e6f0a2fd2d725c021113
JinaneM <jinane.mehdad@gmail.com>
2018-11-13 10:13:00 +0100

    add exo1

--- js/exo1.js
+++ js/exo1.js
@@ -5,3 +5,11 @@
  * 3. Additionner les deux nombres.
  * 4. Afficher le résultat dans #result.
  */
+
+ function Additionner(){
+     const val1= +document.getElementById('firstVal').value;
+     const val2= +document.getElementById('secondVal').value;
+     const result = val1 + val2;
+       return document.getElementById('result').innerHTML = result;
+     
+ }
