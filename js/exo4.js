5db5f49afffe075b16c5abdad4c96f6da0218a31
JinaneM <jinane.mehdad@gmail.com>
2018-11-17 15:12:53 +0100

    exo 4

--- js/exo4.js
+++ js/exo4.js
@@ -7,3 +7,24 @@
  * Astuce : on peut avoir un objet de date en faisant : var now = new Date();
  * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
  */
+
+function checkTime(test){
+    if(test < 10){
+       test = `0${test}`; 
+       console.log(test) ; 
+    }
+    return test;
+}
+function DateHour() {
+    const today = new Date();
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    const sec = today.getSeconds();
+    var fmtMin = checkTime(min);
+    var fmtSec = checkTime(sec);
+    
+    document.getElementById("container").innerHTML = hour + ":" + fmtMin + ":" + fmtSec;
+    setTimeout(function(){ DateHour() }, 500);
+    
+}
+    
